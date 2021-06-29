8128c5d826f64932374118c6369cfb6be25b9c2f
Ellie <ipatlove3@gmail.com>
2021-06-29 14:15:00 +0900

    order manager ver.array

---
+++ ordermanager/orderManager.py
@@ -0,0 +1,34 @@
+'''
+orderManager 클래스를 완성하세요.
+'''
+
+class orderManager :
+    '''
+    주문을 처리하는 class를 작성합니다.
+    '''
+
+    def __init__(self) :
+        '''
+        이 부분은 고치지 마세요.
+        '''
+        self.data = []
+
+    def addOrder(self, orderId) :
+        '''
+        주문번호 orderId를 추가합니다.
+        '''
+        pass
+
+    def removeOrder(self, orderId) :
+        '''
+        주문번호 orderId를 제거합니다.
+        '''
+        pass
+
+    def getOrder(self, orderId) :
+        '''
+        주문번호 orderId가 몇 번째로 처리될지를 반환합니다.
+
+        만약 주문번호 orderId가 존재하지 않으면 -1을 반환합니다. 
+        '''
+        pass
