b66c2a721539d60e54e45a53f2e2130d4559b95d
Ellie <ipatlove3@gmail.com>
2021-06-29 10:52:32 +0900

    sorting task

---
+++ sorting/main.py
@@ -0,0 +1,13 @@
+from maxMachine import sorting
+
+def main():
+    '''
+    이 곳은 수정하지 마세요.
+    '''
+
+    myList = [int(v) for v in input().split()]
+
+    print(sorting(myList))
+
+if __name__ == "__main__":
+    main()
---
+++ sorting/sorting.py
@@ -0,0 +1,46 @@
+'''
+내림차순 정렬하기
+
+단, ‘최댓값 기계’문제에서 작성하였던 자료구조를 이용하여 문제를 풀도록 합니다.
+
+숫자들을 최댓값 기계에 넣고, 최댓값 기계에서 반환되는 숫자들을 받아 출력하면 됩니다.
+
+지시사항
+입력
+첫째줄에 nn개의 정수들이 공백으로 구분되어 입력됩니다. (100≤n≤200000)
+
+출력
+입력된 정수들을 내림차순으로 정렬하여 출력합니다.
+
+1. maxMachine 클래스를 완성하세요.
+2. sorting 함수를 완성하세요.
+'''
+
+class maxMachine :
+
+    def __init__(self) :
+        pass
+
+    def addNumber(self, n) :
+        pass
+
+    def removeNumber(self, n) :
+        pass
+
+    def getMax(self) :
+        pass
+
+def sorting(myList) :
+    '''
+    myList를 내림차순으로 정렬하여 반환하는 함수를 작성하세요.
+
+    예를 들어, myList = [5, 2, 3, 1] 이라면 [5, 3, 2, 1] 을 반환해야 합니다.
+
+    단, maxMachine class를 이용하도록 합니다. 
+    '''
+
+    myMachine = maxMachine()
+
+    result = []
+
+    return result
